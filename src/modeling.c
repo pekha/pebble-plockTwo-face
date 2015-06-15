01d0d045714225b85388d86528563b4589a93f8d
hakkanicko <nicolas.marsal@gmail.com>
2015-06-15 23:57:30 +0200

    Add data

--- src/modeling.c
+++ src/modeling.c
@@ -1,7 +1,21 @@
 #include <pebble.h>
 #include "modeling.h"
 
-clockState time2clockState(struct tm *tick_time, const char* lng){
+static char *zones;
+
+void init(){
+  ResHandle handle_zones = resource_get_handle(RESOURCE_ID_ZONES_FR);
+  ResHandle handle_on = resource_get_handle(RESOURCE_ID_ON_FR);
+  size_t handle_zones_size = resource_size(handle_zones);
+  zones = (char*)malloc(handle_zones_size);
+  resource_load(handle_zones, (uint8_t*)zones, handle_zones_size);
+}
+  
+clockState time2clockState(struct tm *tick_time, language lng){
+  
+  init();
+  char row_size = zones[0];
+  char col_size = zones[1];
   
   //init matrix
   int n = 1;
@@ -10,6 +24,6 @@ clockState time2clockState(struct tm *tick_time, const char* lng){
         state[i] = (bool*) malloc(n * sizeof(bool));
   
   //create structure
-  clockState result = {state, 0, 0, tick_time};
+  clockState result = {state, row_size, col_size, tick_time};
   return result;
 }
--- src/modeling.h
+++ src/modeling.h
@@ -8,4 +8,6 @@ typedef struct {
   struct tm *tick_time;
 } clockState;
   
-clockState time2clockState(struct tm *tick_time, const char* lng);
+typedef enum {LNG_FR, LNG_EN} language;
+
+clockState time2clockState(struct tm *tick_time, language lng);
