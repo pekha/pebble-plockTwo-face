a93f16c5b2624e3207eae82f3357d09c7552034f
hakkanicko <nicolas.marsal@gmail.com>
2015-06-17 16:33:27 +0200

    yeah

--- src/main.c
+++ src/main.c
@@ -65,8 +65,8 @@ static void deinit() {
 
 static void initCurrentLng(){
   int size =  strlen(i18n_get_system_locale());
-  current_lng = malloc(size * sizeof(char));
-  strcpy(current_lng,i18n_get_system_locale());
+  current_lng = "fr_FR"; //malloc(size * sizeof(char));
+  //strcpy(current_lng,i18n_get_system_locale());
   APP_LOG(APP_LOG_LEVEL_DEBUG, "locale : %s", current_lng);
 }
 
